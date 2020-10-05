ad4589cd2a561561ee7e46993c573745ccbf646f
sakiso <sakis4ssy@gmail.com>
2020-10-06 00:47:50 +0900

    都道府県リストコンポーネント作成

--- src/components/team-sidebar.vue
+++ src/components/team-sidebar.vue
@@ -1,11 +1,21 @@
 <template>
-  <v-card tile height="100%">
-    <v-card-title> playerwsssss </v-card-title>
-  </v-card>
+  <v-app>
+    <v-card tile height="100%">
+      <v-card-title> チーム検索条件 </v-card-title>
+      <v-card-text>
+        <prefecturesList></prefecturesList>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
-export default {}
+import PrefecturesList from '@/components/prefectures-list'
+export default {
+  components: {
+    PrefecturesList,
+  },
+}
 </script>
 
 <style></style>
