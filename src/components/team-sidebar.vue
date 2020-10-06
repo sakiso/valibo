1decc7e7764132b99d94cda77a820ddd62bb39d1
sakiso <sakis4ssy@gmail.com>
2020-10-07 01:08:47 +0900

    検索ボタンを整えた（２）

--- src/components/team-sidebar.vue
+++ src/components/team-sidebar.vue
@@ -2,7 +2,16 @@
   <v-app>
     <v-card tile height="100%">
       <v-card-title> チーム検索条件 </v-card-title>
+      <v-divider></v-divider>
+      <v-card-subtitle class="text-center">
+        <v-btn rounded width="70%">
+          <v-icon>mdi-magnify</v-icon>
+          検索
+        </v-btn>
+      </v-card-subtitle>
       <v-card-text>
+        活動エリア
+        <br />
         <prefecturesList></prefecturesList>
       </v-card-text>
     </v-card>
