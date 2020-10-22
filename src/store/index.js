951ee5fb0b2a5e5bfb576c686ae6899f36bbbab9
sakiso <sakis4ssy@gmail.com>
2020-10-23 01:29:16 +0900

    vuex-persistedstate 導入

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//stateをlocalStrageに保存するためのプラグイン
+import createPersistedState from 'vuex-persistedstate'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    role: '',
+  },
+  mutations: {
+    updateRole(state, role) {
+      console.log('stateに渡ってきたrole:', role)
+      if (role === 'admin') {
+        //管理者権限の場合
+        state.role = 'admin'
+        console.log('stateのadminを更新完了')
+      } else if (role === 'user') {
+        //一般ユーザ権限の場合
+        state.role = 'user'
+        console.log('stateのuserを更新完了')
+      } else {
+        //管理者権限がadminでもuserでもない場合（想定されない）
+        state.role = ''
+        console.log('stateのuserをスペースで更新')
+      }
+    },
+    deleteRole(state) {
+      //ログアウト処理時にstateのroleを初期化する
+      state.role = ''
+    },
+  },
   actions: {},
-  modules: {},
+
+  //stateをlocalStrageに保存するためのプラグイン
+  plugins: [
+    createPersistedState({
+      // ストレージのキーを指定。デフォルトではvuexなので他アプリと重複する
+      key: 'valibo',
+
+      // ストレージの種類としてlocalstorageを指定する。
+      storage: window.localStrage,
+    }),
+  ],
 })
