72db0f6668797cc738b75628c4dd838d1003a352
sakiso <sakis4ssy@gmail.com>
2020-10-24 23:34:56 +0900

    tabbarにアイコンを接待した

--- src/components/tabBar.vue
+++ src/components/tabBar.vue
@@ -1,6 +1,7 @@
 <template>
   <v-tabs v-model="tab" background-color="#67b0b2" grow dark>
     <v-tab v-for="item in items" :key="item.tab" @click="switchPage(item.path)">
+      <v-icon class="mr-2">{{ item.icon }}</v-icon>
       {{ item.tab }}
     </v-tab>
   </v-tabs>
@@ -13,9 +14,9 @@ export default {
     return {
       tab: null,
       items: [
-        { tab: 'チーム検索', path: '/teams' },
-        { tab: 'チーム登録', path: '/entry-team' },
-        { tab: 'ログイン', path: '/signin' },
+        { icon: 'mdi-magnify', tab: 'チーム検索', path: '/teams' },
+        { icon: 'mdi-account-group', tab: 'チーム登録', path: '/entry-team' },
+        { icon: 'mdi-login-variant', tab: 'ログイン', path: '/signin' },
       ],
     }
   },
