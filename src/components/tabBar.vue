06a8c773badfcb927922f4ee7fff61f9abaf8952
sakiso <sakis4ssy@gmail.com>
2020-11-03 20:50:48 +0900

    pathを元にtabsの配列を返す計算プロパティ追加

--- src/components/tabBar.vue
+++ src/components/tabBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-tabs value="tabsValue" v-model="tab" background-color="#67b0b2" grow dark>
+  <v-tabs v-model="tabIndex" background-color="#67b0b2" grow dark>
     <v-tab v-for="item in items" :key="item.tab" @click="switchPage(item.path)">
       <v-icon class="mr-2">{{ item.icon }}</v-icon>
       <font v-if="!$vuetify.breakpoint.xsOnly">{{ item.tab }}</font>
@@ -26,18 +26,17 @@ export default {
     }
   },
 
-  mounted: function () {
-    //現在のpathに応じてアクティブなタブを切り替える
-    //現在のpathがitmes配列の何番目のpathと一致するかで、アクティブにすべきタブを決める
-    if (this.$route.path === this.items[0].path) {
-      this.tab = 0
-    } else if (this.$route.path === this.items[1].path) {
-      this.tab = 1
-    } else if (this.$route.path === this.items[2].path) {
-      this.tab = 2
-    } else {
-      return
-    }
+  computed: {
+    tabIndex: {
+      get: function () {
+        console.log(this.items.findIndex((el) => el.path === this.$route.path))
+        //現在のpathでitemsを検索し、インデックスを取得
+        return this.items.indexOf(this.$route.path)
+      },
+      set: function () {
+        //setterは特になし
+      },
+    },
   },
 
   methods: {
