9560cc5a968872ec13b89b786031ec07e549b0b6
sakiso <sakis4ssy@gmail.com>
2020-11-09 01:42:38 +0900

    メッセージ画面のガワ作成

--- src/views/message.vue
+++ src/views/message.vue
@@ -3,11 +3,22 @@
     <v-content>
       <v-container ma-0 pt-0 pb-0 fluid class="height-100per">
         <v-row class="height-100per">
-          <v-col cols="2">
-            メッセージ一覧
-            <v-divider vertical></v-divider>
+          <v-col cols="3">
+            <v-card height="100%">
+              <v-card-title> メッセージ一覧 </v-card-title>
+              <v-card-text>
+                <v-list>
+                  <v-list-item>a</v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-card>
           </v-col>
-          <v-col cols="10">メッセージ詳細</v-col>
+          <v-col cols="9">
+            <v-card height="100%">
+              <v-card-title> メッセージ詳細 </v-card-title>
+              <v-card-text> aaa </v-card-text>
+            </v-card>
+          </v-col>
         </v-row>
       </v-container>
     </v-content>
