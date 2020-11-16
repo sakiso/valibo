84202f83b6082f37edcf38095cc3a107026ce7cc
sakiso <sakis4ssy@gmail.com>
2020-11-17 00:40:43 +0900

    messageコレクションから一括で取得する状態

--- src/components/message-detail.vue
+++ src/components/message-detail.vue
@@ -1,15 +1,35 @@
 <template>
   <v-card height="100%">
     <v-card-title> メッセージ詳細 </v-card-title>
-    <v-card-text> abc </v-card-text>
+    <v-card-text> {{ messages }} </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { db } from '@/plugins/firebase'
+
 export default {
   name: 'MessageDetail',
   data() {
-    return {}
+    return {
+      messages: {},
+    }
+  },
+
+  created: function () {
+    //Firestoreからデータ取得
+    //messageコレクションへの参照
+    this.messageRef = db.collection('message')
+
+    this.messageRef.onSnapshot((querySnapshot) => {
+      //リスナー配置し、Firestoreのデータが更新されるたびに以下の関数を実行
+      //データ取得処理
+      const obj = {}
+      querySnapshot.forEach((doc) => {
+        obj[doc.id] = doc.data()
+      })
+      this.messages = obj
+    })
   },
 }
 </script>
