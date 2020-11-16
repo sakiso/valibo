186dc5479c94d14313924bbc175c9ddab2676c77
sakiso <sakis4ssy@gmail.com>
2020-11-17 00:56:16 +0900

    取得したメッセージの数だけv-forでカードを量産 #71 next: サイドバーにサマリを表示する

--- src/components/message-detail.vue
+++ src/components/message-detail.vue
@@ -1,8 +1,14 @@
 <template>
-  <v-card height="100%">
-    <v-card-title> メッセージ詳細 </v-card-title>
-    <v-card-text> {{ messages }} </v-card-text>
-  </v-card>
+  <v-container pa-0 ma-0>
+    <v-row>
+      <v-col v-for="message in messages" :key="message.key">
+        <v-card height="100%">
+          <v-card-title> メッセージ詳細 </v-card-title>
+          <v-card-text> {{ message }} </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
