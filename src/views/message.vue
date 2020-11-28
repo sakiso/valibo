3ed221dac4e9e088f56f1256dc79e13e2b9e415c
sakiso <sakis4ssy@gmail.com>
2020-11-28 15:39:25 +0900

    colsを指定すると右に行くには行ったが…

--- src/views/message.vue
+++ src/views/message.vue
@@ -21,10 +21,15 @@
                   color="light-blue"
                 >
                   <v-card-text>
-                    <v-btn large color="#F48D97" dark>
-                      <v-icon> mdi-send-outline </v-icon>
-                      メッセージを送る
-                    </v-btn>
+                    <v-row justify="end">
+                      <v-col cols="1"> s </v-col>
+                      <v-col cols="4">
+                        <v-btn large color="#F48D97" dark>
+                          <v-icon> mdi-send-outline </v-icon>
+                          メッセージを送る
+                        </v-btn>
+                      </v-col>
+                    </v-row>
                   </v-card-text>
                 </v-card>
               </v-col>
